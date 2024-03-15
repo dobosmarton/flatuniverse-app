f48a4817b2a1f830362c8a6a00088fdb13bd9a47
Marton Dobos <marton.dobos@otpeco.com>
2024-03-15 22:16:49 +0100

    fix: sync schema validation fix

--- jobs/events.ts
+++ jobs/events.ts
@@ -1,5 +1,5 @@
-import { z } from 'zod';
 import { metadataSchema } from '@/lib/oai-pmh/schema';
+import { z } from 'zod';
 
 export enum Events {
   research_sync = 'research_sync',
@@ -21,7 +21,54 @@ export const researchSyncRetryPayloadSchema = researchSyncPayloadSchema.extend({
   retryCount: z.number(),
 });
 
+const authorSchema = z.object({
+  name: z.string(),
+});
+
+const linkSchema = z
+  .object({
+    href: z.string(),
+    rel: z.string(),
+    type: z.string(),
+    title: z.string(),
+  })
+  .transform((link) => ({
+    ...link,
+    type: link.type,
+    title: link.title,
+  }));
+
+const categorySchema = z.object({
+  isPrimary: z.boolean(),
+  shortName: z.string(),
+  fullName: z.string(),
+  groupName: z.string(),
+});
+
+const metadataBatchSchema = z
+  .object({
+    id: z.string(),
+    doi: z.string().optional(),
+    title: z.string(),
+    comment: z.string().optional(),
+    abstract: z.string(),
+    created: z.string(),
+    updated: z.string(),
+    published: z.string(),
+    links: z.array(linkSchema),
+    authors: z.array(authorSchema),
+    categories: z.array(categorySchema),
+  })
+  .transform((metadata) => ({
+    ...metadata,
+    comment: metadata.comment,
+    updated: new Date(metadata.updated),
+    published: new Date(metadata.published),
+  }));
+
 export const addArticleMetadaBatchPayloadSchema = z.object({
   batchIndex: z.number(),
-  batch: z.array(metadataSchema),
+  batch: z.array(metadataBatchSchema),
 });
+
+export type MetadataBatch = z.infer<typeof metadataBatchSchema>;
