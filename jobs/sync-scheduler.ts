7f2bbe7c9ca080d61a2d3aabbfffe8fe179b8d04
Marton Dobos <marton.dobos@otpeco.com>
2024-03-15 21:22:26 +0100

    feat: schedule updates

--- jobs/sync-scheduler.ts
+++ jobs/sync-scheduler.ts
@@ -3,17 +3,16 @@ import { client } from '@/trigger';
 import { cronTrigger } from '@trigger.dev/sdk';
 import { Events } from './events';
 
-const startDate = '2024-03-12';
-const untilDate = '2024-03-15';
-
 client.defineJob({
   id: 'research-sync-scheduler',
   name: 'Research Sync Scheduler',
   version: '0.0.1',
   trigger: cronTrigger({
-    cron: '0 4 * * *', //every day at 4:00
+    cron: '0 */3 * * *', // At minute 0 past every 3rd hour
   }),
   run: async (payload, io, ctx) => {
+    const startDate = new Date().toISOString().split('T')[0];
+
     const jobId = randomUUID();
     await io.sendEvent(`${Events.research_sync}-${jobId}`, {
       name: Events.research_sync,
@@ -22,7 +21,6 @@ client.defineJob({
       },
       payload: {
         startDate,
-        untilDate,
       },
     });
 
