ae42e76641049b4d63c7ad77e9c1a53b81201345
Marton Dobos <marton.dobos@otpeco.com>
2024-03-18 19:39:04 +0100

    feat: ai content refactor

--- jobs/ai/tasks/generate-embedding.ts
+++ jobs/ai/tasks/generate-embedding.ts
@@ -14,84 +14,84 @@ export const generateContent = async (
     external_id: string;
   }
 ) => {
-  return await io.runTask(`${cacheKey}`, async () => {
+  const pdfLink = await io.runTask(`${cacheKey}-get-article-pdf`, async () => {
     await io.logger.info(`Generate AI Content - Id: ${item.id}`, {
       time: new Date().toISOString(),
     });
 
-    const pdfLink = await articleMetadataService.getArticlePdfLink(item.id);
+    return await articleMetadataService.getArticlePdfLink(item.id);
+  });
 
-    if (!pdfLink) {
-      await io.logger.info(`PDF not found for metadata id: ${item.id}`, {
-        time: new Date().toISOString(),
-      });
-      return;
-    }
-
-    const pdfJsonData = await io.runTask(`${cacheKey}-load-pdf`, async (task) => {
-      await io.logger.info(`Load PDF - Index: ${item.id}`, {
-        time: new Date().toISOString(),
-      });
-
-      const pdfDocs = await loadPDF(pdfLink, { metadata_id: item.id });
-
-      await io.logger.info(`PDF file loaded successfully! Length: ${pdfDocs.length}`, {
-        time: new Date().toISOString(),
-      });
-
-      // tasks can return JSON serializable data
-      return JSON.stringify({ doc: pdfDocs });
+  if (!pdfLink) {
+    await io.logger.info(`PDF not found for metadata id: ${item.id}`, {
+      time: new Date().toISOString(),
     });
+    return;
+  }
 
-    await io.runTask(`${cacheKey}-add-embeddings`, async (task) => {
-      await io.logger.info(`Add Embeddings - Index: ${item.id}`, {
-        time: new Date().toISOString(),
-      });
-
-      const pdfDocs = JSON.parse(pdfJsonData).doc;
-
-      await addNewEmbeddings(pdfDocs);
-
-      await io.logger.info(`Add Embeddings - Done`, {
-        time: new Date().toISOString(),
-      });
-
-      return task;
-    });
-
-    const generatedSummary = await io.runTask(`${cacheKey}-generate-summary`, async (task) => {
-      const pdfDocs = JSON.parse(pdfJsonData).doc;
-
-      const summary = await getSummaryByDocuments(pdfDocs);
-
-      await io.logger.info(`Summary generated successfully! Length: ${summary?.length}`, {
-        time: new Date().toISOString(),
-      });
-
-      return summary;
-    });
-
-    await io.runTask(`${cacheKey}-add-summary`, async (task) => {
-      if (!generatedSummary) {
-        await io.logger.info(`Summary not generated`, {
-          time: new Date().toISOString(),
-        });
-        return;
-      }
-
-      await articleService.addGeneratedSummary(item.id, generatedSummary);
-
-      await io.logger.info(`Generate AI Content - Added`, {
-        time: new Date().toISOString(),
-      });
-
-      return task;
-    });
-
-    await io.logger.info(`Generate AI Content - Done`, {
+  const pdfJsonData = await io.runTask(`${cacheKey}-load-pdf`, async () => {
+    await io.logger.info(`Load PDF - Index: ${item.id}`, {
       time: new Date().toISOString(),
     });
 
-    return { item };
+    const pdfDocs = await loadPDF(pdfLink, { metadata_id: item.id });
+
+    await io.logger.info(`PDF file loaded successfully! Length: ${pdfDocs.length}`, {
+      time: new Date().toISOString(),
+    });
+
+    // tasks can return JSON serializable data
+    return JSON.stringify({ doc: pdfDocs });
   });
+
+  await io.runTask(`${cacheKey}-add-embeddings`, async (task) => {
+    await io.logger.info(`Add Embeddings - Index: ${item.id}`, {
+      time: new Date().toISOString(),
+    });
+
+    const pdfDocs = JSON.parse(pdfJsonData).doc;
+
+    await addNewEmbeddings(pdfDocs);
+
+    await io.logger.info(`Add Embeddings - Done`, {
+      time: new Date().toISOString(),
+    });
+
+    return task;
+  });
+
+  const generatedSummary = await io.runTask(`${cacheKey}-generate-summary`, async (task) => {
+    const pdfDocs = JSON.parse(pdfJsonData).doc;
+
+    const summary = await getSummaryByDocuments(pdfDocs);
+
+    await io.logger.info(`Summary generated successfully! Length: ${summary?.length}`, {
+      time: new Date().toISOString(),
+    });
+
+    return summary;
+  });
+
+  if (!generatedSummary) {
+    await io.logger.info(`Summary not generated`, {
+      time: new Date().toISOString(),
+    });
+    return;
+  }
+
+  await io.runTask(`${cacheKey}-add-summary`, async (task) => {
+    await articleService.addGeneratedSummary(item.id, generatedSummary);
+
+    await io.logger.info(`Generate AI Content - Added`, {
+      time: new Date().toISOString(),
+    });
+
+    return task;
+  });
+
+  await io.logger.info(`Generate AI Content - Done`, {
+    time: new Date().toISOString(),
+  });
+
+  return { item };
 };
