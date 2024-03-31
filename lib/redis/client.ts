095de6b15d8d1de198b45c3faaea5a891a0449c4
Marton Dobos <marton.dobos@otpeco.com>
2024-03-31 18:32:53 +0200

    chore: env updates

--- lib/redis/client.ts
+++ lib/redis/client.ts
@@ -2,5 +2,5 @@ import { Redis } from '@upstash/redis';
 
 export const client = new Redis({
   url: process.env.REDIS_REST_URL ?? '',
-  token: process.env.REDUS_REST_TOKEN ?? '',
+  token: process.env.REDIS_REST_TOKEN ?? '',
 });
