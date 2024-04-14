3146b83173c1d9970057faa6708cbbad618ebbdb
Marton Dobos <marton.dobos@otpeco.com>
2024-04-14 21:49:54 +0200

    chore: remove unused schema & filter store

--- stores/index.ts
+++ stores/index.ts
@@ -1,17 +1,15 @@
 import { create } from 'zustand';
 import { AIModeSlice, createAIModeSlice } from './ai-mode';
-import { ArticleFilters, createArticleFiltersSlice } from './article-filters';
 import { NewsletterSlice, createNewsletterSlice } from './newsletter';
 import { ViewSettingsSlice, createViewSettingsSlice } from './view-settings';
 import { persist } from 'zustand/middleware';
 
-export type Store = AIModeSlice & ArticleFilters & NewsletterSlice & ViewSettingsSlice;
+export type Store = AIModeSlice & NewsletterSlice & ViewSettingsSlice;
 
 export const useBoundStore = create<Store>()(
   persist(
     (...a) => ({
       ...createAIModeSlice(...a),
-      ...createArticleFiltersSlice(...a),
       ...createNewsletterSlice(...a),
       ...createViewSettingsSlice(...a),
     }),
