26fc59357edbad63d0499e9d402c86fa4896bc64
Marton Dobos <marton.dobos@otpeco.com>
2024-06-13 19:16:00 +0200

    feat: disable trigger.dev v2 schedules

--- jobs/newsletter/send-weekly-newsletter.ts
+++ jobs/newsletter/send-weekly-newsletter.ts
@@ -1,16 +1,16 @@
 import { client } from '@/trigger';
-import { invokeTrigger } from '@trigger.dev/sdk';
+import { cronTrigger } from '@trigger.dev/sdk';
 import * as newsletterService from '@/lib/newsletter';
 
 client.defineJob({
   id: 'weekly-email-newsletter',
   name: 'Weekly Email Newsletter Job',
   version: '0.0.1',
-  trigger: invokeTrigger(),
-  // trigger: cronTrigger({
-  // Every Saturday at 8:00 AM
-  //  cron: '0 8 * * 6',
-  //}),
+  trigger: cronTrigger({
+    // Every Saturday at 8:00 AM
+    cron: '0 8 * * 6',
+  }),
+  enabled: false,
   run: async (payload, io, ctx) => {
     await io.runTask(`send-weekly-newsletter`, async () => {
       await newsletterService.sendWeeklySummaryEmail();
--- jobs/sync-scheduler.ts
+++ jobs/sync-scheduler.ts
@@ -1,17 +1,16 @@
 import { randomUUID } from 'crypto';
 import { client } from '@/trigger';
-import { invokeTrigger } from '@trigger.dev/sdk';
+import { cronTrigger } from '@trigger.dev/sdk';
 import { Events } from './events';
 
 client.defineJob({
   id: 'research-sync-scheduler',
   name: 'Research Sync Scheduler',
   version: '0.0.1',
-  trigger: invokeTrigger(),
-  //trigger: cronTrigger({
-  // cron: '0 6 * * 1', // At minute 0 past every 6th hour
-  //  cron: '0 */6 * * *', // At minute 0 past every 6th hour
-  //}),
+  trigger: cronTrigger({
+    cron: '0 */6 * * *', // At minute 0 past every 6th hour
+  }),
+  enabled: false,
   run: async (payload, io, ctx) => {
     const startDate = new Date();
     startDate.setDate(startDate.getDate() - 1);
