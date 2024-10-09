2a466390fa4e6111763a8e2bac728c91b625777d
Marton Dobos <marton.dobos@otpeco.com>
2024-10-09 21:45:45 +0200

    fix: remove jobs import

--- app/api/trigger/route.ts
+++ app/api/trigger/route.ts
@@ -1,8 +1,6 @@
 import { createAppRoute } from '@trigger.dev/nextjs';
 import { client } from '@/trigger';
 
-import '@/jobs';
-
 //this route is used to send and receive data with Trigger.dev
 export const { POST, dynamic } = createAppRoute(client);
 
