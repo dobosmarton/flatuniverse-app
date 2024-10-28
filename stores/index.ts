1562225e63512a417e943188f10f90c8b519a567
Marton Dobos <marton.dobos@otpeco.com>
2024-10-28 17:34:19 +0100

    feat: context chat store

--- stores/index.ts
+++ stores/index.ts
@@ -2,11 +2,13 @@ import { create } from 'zustand';
 import { AIModeSlice, createAIModeSlice } from './ai-mode';
 import { NewsletterSlice, createNewsletterSlice } from './newsletter';
 import { ViewSettingsSlice, createViewSettingsSlice } from './view-settings';
+import { ContextChatSlice, createContextChatSlice } from './context-chat';
 
-export type Store = AIModeSlice & NewsletterSlice & ViewSettingsSlice;
+export type Store = AIModeSlice & NewsletterSlice & ViewSettingsSlice & ContextChatSlice;
 
 export const useBoundStore = create<Store>()((...a) => ({
   ...createAIModeSlice(...a),
   ...createNewsletterSlice(...a),
   ...createViewSettingsSlice(...a),
+  ...createContextChatSlice(...a),
 }));
